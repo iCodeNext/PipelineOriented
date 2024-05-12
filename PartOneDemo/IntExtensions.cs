f570a7eb31d127fc7da36b8bf68bab01232bd9ca
mohammadKarimi <mha.karimi@gmail.com>
2024-05-12 14:14:26 +0330

    noting chnaged

--- PartOneDemo/IntExtensions.cs
+++ PartOneDemo/IntExtensions.cs
@@ -1,4 +1,7 @@
-﻿namespace PartOneDemo;
+﻿using System.Collections.Generic;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace PartOneDemo;
 public static class IntExtensions
 {
     public static int Add1(this int x) => x + 1;
